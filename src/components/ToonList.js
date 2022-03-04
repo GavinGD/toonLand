b342af337dac32dfa341c7c6802305ce1988fd8b
Gavin Knoke <gavink1279@hotmail.com>
2022-03-04 11:20:20 -0800

    Added repo

---
+++ src/components/ToonList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const ToonList = (param) => {
+
+  const [toonInfo, setToonInfo] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`https://api4all.azurewebsites.net/api/people/`);
+      const body = await result.json();
+      setToonInfo(body);
+    }
+    fetchData();
+  }, []);
+
+  var others = toonInfo;
+
+  if (param !== undefined) {
+    others = Object.values(toonInfo).filter(p => p.id !== +param.exceptId);
+  }
+
+
+  return (
+    <>
+      {others.map((person, key) => (
+        <Link key={key} to={`/detail/${person.id}`}>
+          <h6>{person.id} {person.firstName} {person.lastName}</h6>
+        </Link>
+      ))}
+    </>
+  )
+}
+export default ToonList; 
---
+++ src/pages/ToonDetailPage.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import ToonList from '../components/ToonList';
+import NotFoundPage from './NotFoundPage';
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react'
+
+const ToonDetailPage = () => {
+  const { id } = useParams();
+
+  const [toonInfo, setToonInfo] = useState({
+    votes: 0,
+    id: 0,
+    firstName: '',
+    lastName: ''
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`https://api4all.azurewebsites.net/api/people/${id}`);
+      const body = await result.json();
+      // console.log(body)
+      setToonInfo(body);
+    }
+    fetchData();
+  }, [id]);
+
+  if (!toonInfo) return <NotFoundPage />
+
+  return (
+    <>
+      <h4 className="text-info">{toonInfo.id}. {toonInfo.firstName} {toonInfo.lastName}</h4>
+      <p>This cartoon character has received {toonInfo.votes} votes.</p>
+      <table style={{ "width": "90%", "margin": "auto" }}>
+        <tbody>
+          <tr>
+            <td style={{ "width": "15%", "verticalAlign": "top" }}>
+              <img className="rounded img-responsive pull-right img-thumbnail float-left"
+                style={{ "width": "50%" }}
+                src={`${toonInfo.pictureUrl}`} alt={`${toonInfo.firstName} ${toonInfo.lastName}`} />
+            </td>
+            <td style={{ "width": "65%", "verticalAlign": "top" }}>
+              <p><b>Occupation: </b>{toonInfo.occupation}</p>
+              <p><b>Gender: </b>{toonInfo.gender}</p>
+            </td>
+            <td style={{ "width": "20%", "verticalAlign": "top" }}>
+              <h3>Others:</h3>
+              <ToonList exceptId={toonInfo.id} />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+    </>
+  );
+}
+export default ToonDetailPage;
